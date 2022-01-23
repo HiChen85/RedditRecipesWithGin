54c69fb4c2edbbbf3c45ae9260b6be592a676cbc
HiChen85 <hccc_zhang@outlook.com>
2022-01-23 17:09:05 +0800

    refactor the handlers with mongoDB

--- models/recipes.go
+++ models/recipes.go
@@ -1,12 +1,15 @@
 package models
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name" bson:"name"`
-	Tags         []string  `json:"tags" bson:"tags"`
-	Ingredients  []string  `json:"ingredients" bson:"ingredients"`
-	Instructions []string  `json:"instructions" bson:"instructions"`
-	PublishAt    time.Time `json:"publishAt"`
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	Name         string             `json:"name" bson:"name"`
+	Tags         []string           `json:"tags" bson:"tags"`
+	Ingredients  []string           `json:"ingredients" bson:"ingredients"`
+	Instructions []string           `json:"instructions" bson:"instructions"`
+	PublishAt    time.Time          `json:"publishAt" bson:"publishAt""`
 }
